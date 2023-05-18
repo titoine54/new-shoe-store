838f10d85d542313aa06a31e53d15dc9f883f194
titoine54 <titoine54@hotmail.com>
2023-05-18 15:56:24 -0400

    feat: setting up rails app

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  root "inventory#index"
+  get "/inventory", to: "inventory#index"
+end
