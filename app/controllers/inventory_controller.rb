c2b905886c27948df2e41bff21c0eb9a82aa805c
titoine54 <titoine54@hotmail.com>
2023-05-19 01:01:01 -0400

    feat: added low-high inventory top 3

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -5,6 +5,8 @@ class InventoryController < ApplicationController
     @stores = Store.all
     @models = Model.all
     @alerts = Inventory.where("inventory < ? ", 10)
+    @high_inventory = get_high_inventory
+    @low_inventory = get_low_inventory
   end
 
   def add
@@ -14,5 +16,21 @@ class InventoryController < ApplicationController
     row = Inventory.find_or_create_by(model_id: model.id, store_id: store.id)
     row.inventory = params['inventory']
     row.save
+  end
+
+  private
+
+  def get_high_inventory
+    high_inventory_models = Inventory.group(:model_id).average(:inventory)
+
+    top_high_inventory_models = high_inventory_models.sort_by { |model| -model[1] }.first(3)
+    top_high_inventory_models.map { |model, inventory| [Model.find_by_id(model).name, inventory.to_i] }
+  end
+
+  def get_low_inventory
+    low_inventory_models = Inventory.group(:model_id).average(:inventory)
+
+    top_low_inventory_models = low_inventory_models.sort_by { |model| model[1] }.first(3)
+    top_low_inventory_models.map { |model, inventory| [Model.find_by_id(model).name, inventory.to_i] }
   end
 end
