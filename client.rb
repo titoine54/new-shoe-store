a6cc55b369813971b453c7f114bc9c186a911ef8
titoine54 <titoine54@hotmail.com>
2023-05-18 22:43:12 -0400

    refactor: cleanup client service

--- client.rb
+++ client.rb
@@ -5,24 +5,6 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-class Inventory
-  def initialize
-    @stores = {}
-  end
-
-  def add(message)
-    if @stores.include?(message.store)
-      # if stores[message.store].include?(message.model)
-      @stores[message.store][message.model] = message.inventory
-      # else
-      #   stores[message.store][message.model] = model.inventory
-    else
-      @stores[message.store] = { message.model => message.inventory }
-    end
-    puts @stores
-  end
-end
-
 class Message
   attr_accessor :store, :model, :inventory
 
@@ -30,22 +12,8 @@ class Message
     @store = store
     @model = model
     @inventory = inventory
-
-    check_inventory
-  end
-
-  private
-
-  def check_inventory
-    if @inventory == 0
-      p "#{model} in #{store} is out of stock"
-    elsif @inventory < 10
-      p "Inventory low for #{model} in #{store}. Only #{inventory} left!"
-    end
   end
 end
-
-inventory = Inventory.new
 
 EM.run {
   ws = Faye::WebSocket::Client.new('ws://localhost:8080/')
@@ -63,14 +31,10 @@ EM.run {
       :inventory => message.inventory,
     }
 
-    # Create the HTTP objects
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Post.new(uri.request_uri, header)
     request.body = payload.to_json
 
-    # Send the request
     response = http.request(request)
-
-    # inventory.add(message)
   end
 }
